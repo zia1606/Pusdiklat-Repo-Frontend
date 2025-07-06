e68a21b42dd6f9046a22dab79fcd81c3db804489
Fauziah Filda Mufarrihati <222112055@stis.ac.id>
2025-07-06 12:18:16 +0700

    keduapuluh4

--- app.vue
+++ app.vue
@@ -7,9 +7,9 @@
 <script setup>
 import { onBeforeMount } from 'vue';
 
-// app.vue - Pindahkan preload ke mounted untuk menghindari blocking render awal
 onMounted(() => {
   if (process.client) {
+    // Preload PDFTron dan watermark
     const preload = Promise.all([
       import('@pdftron/pdfjs-express-viewer').then(module => {
         window.__PDFJSExpress = module.default;
@@ -20,24 +20,6 @@ onMounted(() => {
         img.onload = resolve;
       })
     ]).catch(console.error);
-  }
-});
-
-onBeforeMount(() => {
-  if (process.client) {
-    // Preload PDF.js Express dan library penting lainnya
-    const preloadResources = [
-      import('@pdftron/pdfjs-express-viewer').then(module => {
-        window.__PDFJSExpress = module.default;
-      }),
-      new Promise(resolve => {
-        const img = new Image();
-        img.src = '/files/BPS.png'; // Preload watermark
-        img.onload = resolve;
-      })
-    ];
-    
-    Promise.allSettled(preloadResources).catch(console.error);
   }
 });
 </script>
